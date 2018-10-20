80577fb71b97a52ffc8bf01214829e94fb9e731e
otavioomota <41590014+otavioomota@users.noreply.github.com>
2018-10-20 16:48:30 -0300

    Updates

---
+++ precache-manifest.872dfe840d4058dec849e8902c97717a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "56a2737bd7efd1daad94",
+    "url": "/robofriends/static/js/main.56a2737b.chunk.js"
+  },
+  {
+    "revision": "722a7d6ad09c40a70e84",
+    "url": "/robofriends/static/js/1.722a7d6a.chunk.js"
+  },
+  {
+    "revision": "56a2737bd7efd1daad94",
+    "url": "/robofriends/static/css/main.46f31b3f.chunk.css"
+  },
+  {
+    "revision": "722a7d6ad09c40a70e84",
+    "url": "/robofriends/static/css/1.4ef73cbc.chunk.css"
+  },
+  {
+    "revision": "1db0502ab1eda4d2a5002ba045dca0f6",
+    "url": "/robofriends/index.html"
+  }
+];
